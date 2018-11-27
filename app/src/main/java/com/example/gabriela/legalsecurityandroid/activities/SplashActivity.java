c04d2fb3756937ba4dd5ce1aab3032c5bf83dd46
Gabriela <graciela_ardiles@itrsa.com.ar>
2018-11-27 14:33:35 -0300

    Fixed permissions for api 23

--- app/src/main/java/com/example/gabriela/legalsecurityandroid/activities/SplashActivity.java
+++ app/src/main/java/com/example/gabriela/legalsecurityandroid/activities/SplashActivity.java
@@ -29,10 +29,6 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-
-        // Status bar
-       //  this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
