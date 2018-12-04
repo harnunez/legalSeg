6c8373e50c06c9eb6ebabbfb4ca0339c60a67f19
Gabriela <graciela_ardiles@itrsa.com.ar>
2018-12-04 10:56:36 -0300

    Fixed comment toast

--- app/src/main/java/com/example/gabriela/legalsecurityandroid/activities/SelectUserActivity.java
+++ app/src/main/java/com/example/gabriela/legalsecurityandroid/activities/SelectUserActivity.java
@@ -57,7 +57,7 @@ public class SelectUserActivity extends AppCompatActivity {
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(SelectUserActivity.this, "Position: " + position, Toast.LENGTH_SHORT).show();
+                // Toast.makeText(SelectUserActivity.this, "Position: " + position, Toast.LENGTH_SHORT).show();
                 startNewActivity(userLogued.bills[position], userLogued.clientId);
             }
         });
