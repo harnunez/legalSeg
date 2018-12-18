d665768e7004abda83ed541eb83ce12275d0a58e
Jonas Lopez <jonas_lopez@intranet.itresources>
2018-12-18 17:46:45 -0300

    Agrego el metodo warningDialog a la clase Util

--- app/src/main/java/com/example/gabriela/legalsecurityandroid/Utils/Util.java
+++ app/src/main/java/com/example/gabriela/legalsecurityandroid/Utils/Util.java
@@ -1,14 +1,34 @@
 package com.example.gabriela.legalsecurityandroid.Utils;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 
-public class Util {
-    public static void  alertError(String message, Context context) {
+import com.example.gabriela.legalsecurityandroid.R;
+
+public class Util extends AppCompatActivity {
+    public static void  alertError(String message, final Context context) {
         AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(context);
         dlgAlert.setMessage(message);
         dlgAlert.setTitle("Error");
+        dlgAlert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                ActivityCompat.finishAffinity((Activity) context);
+
+            }
+        });
+        dlgAlert.setCancelable(true);
+        dlgAlert.create().show();
+    }
+
+    public static void warningDialog(String message, final Context context){
+        AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(context);
+        dlgAlert.setMessage(message);
+        dlgAlert.setTitle(R.string.warning_title);
         dlgAlert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
                 dialog.dismiss();
@@ -16,6 +36,6 @@ public class Util {
         });
         dlgAlert.setCancelable(true);
         dlgAlert.create().show();
-    }
 
+    }
 }
