1aa771edb25b86f4abccf826a861759459869b6d
jlopez <jonas_lopez@intranet.itresources>
2019-01-10 18:02:03 -0300

    Add GPS high accuracy Google services

--- app/src/main/java/com/example/gabriela/legalsecurityandroid/activities/HomeActivity.java
+++ app/src/main/java/com/example/gabriela/legalsecurityandroid/activities/HomeActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,6 +24,14 @@ import com.example.gabriela.legalsecurityandroid.Utils.Util;
 import com.example.gabriela.legalsecurityandroid.interfaces.doConnectionEvent;
 import com.example.gabriela.legalsecurityandroid.models.EventModel;
 import com.example.gabriela.legalsecurityandroid.services.VolleyImplementation;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -42,12 +51,16 @@ public class HomeActivity extends AppCompatActivity {
     private static final String EVENT_ENTER_HOME = "3";
     private static final String EVENT_LEAVE_HOME = "2";
     private final int ACCESS_FINE_LOCATION_CODE = 100;
+    private final int ACCESS_COARSE_LOCATION = 102;
+    private GoogleApiClient mGoogleApiClient;
+    private static final int REQUEST_CHECK_SETTINGS = 0x1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_home );
         initProperties();
+        initGoogleAPIClient();
         executeActionButtons();
     }
 
@@ -67,25 +80,21 @@ public class HomeActivity extends AppCompatActivity {
         executeShutDown();
     }
 
-    private void checkAppLocationPermisson(final String eventSelected) {
-        if( Util.checkCurrentAndroidVersion()){
-            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, ACCESS_FINE_LOCATION_CODE );
-        }
-        else {
-            checkAppProviders();
+
+    private void checkAppLocationPermisson() {
+        if (Util.checkCurrentAndroidVersion()) {
+            requestPermissions( new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, ACCESS_FINE_LOCATION_CODE );
+        } else {
+            checkGpsSettings();
         }
     }
 
     private void checkAppProviders() {
-        if(! Util.isGPSEnable(HomeActivity.this)){
+        if (!NetworkUtil.isNetworkEnable( HomeActivity.this )) {
             endRunningServiceCall();
-            Util.warningDialog( getResources().getString( R.string.warning_gps ), HomeActivity.this);
+            Util.warningDialog( getResources().getString( R.string.warning_connection ), HomeActivity.this );
         }
-        else if(! NetworkUtil.isNetworkEnable( HomeActivity.this )){
-            endRunningServiceCall();
-            Util.warningDialog( getResources().getString( R.string.warning_connection ), HomeActivity.this);
-        }
-        else{
+        else {
             executeService(eventSelected);
         }
     }
@@ -97,9 +106,8 @@ public class HomeActivity extends AppCompatActivity {
                 if(!runningServiceCall){
                     startRunningServiceCall();
                     eventSelected = EVENT_ENTER_HOME;
-                    checkAppLocationPermisson(eventSelected);
+                    checkAppLocationPermisson();
                 }
-
             }
         });
     }
@@ -114,14 +122,13 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        String permission = permissions[0];
+        int result = grantResults[0];
         switch (requestCode) {
-            case ACCESS_FINE_LOCATION_CODE:
-                String permission = permissions[0];
-                int result = grantResults[0];
-
+            case (ACCESS_FINE_LOCATION_CODE ):
                 if(permission.equals(Manifest.permission.ACCESS_FINE_LOCATION)){
                     if(result == PackageManager.PERMISSION_GRANTED) {
-                        checkAppProviders();
+                        checkGpsSettings();
                     }
                     else{
                         Toast.makeText(this, "Es necesario habilitar este permiso para que funcione la aplicación", Toast.LENGTH_LONG).show();
@@ -134,7 +141,6 @@ public class HomeActivity extends AppCompatActivity {
         }
     }
 
-    // Event leave
     private void executeEventLeaveHome() {
         outHome.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -142,19 +148,17 @@ public class HomeActivity extends AppCompatActivity {
                 if(!runningServiceCall){
                     startRunningServiceCall();
                     eventSelected = EVENT_LEAVE_HOME;
-                    checkAppLocationPermisson(eventSelected);
+                    checkAppLocationPermisson();
                 }
             }
         });
     }
 
-    // EventsWS
     public void executeService(final String eventSelected) {
         VolleyImplementation vimp = new VolleyImplementation(this, new doConnectionEvent() {
             @Override
             public void onOk(JSONObject response) {
                 Log.d("Response", response.toString());
-                // TODO:
                 Gson gson = new GsonBuilder().create();
                 EventModel event = gson.fromJson(response.toString(), EventModel.class);
                 Log.d("EventModel", event.toString());
@@ -198,13 +202,11 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private  void showNewActivity(String event) {
-
         Intent myIntent = new Intent(HomeActivity.this, InHomeActivity.class);
         myIntent.putExtra("event", event);
         myIntent.putExtra("userName", useNameSelect);
         myIntent.putExtra("idCliente", idCliente);
         startActivity(myIntent);
-        finish();
     }
 
     private void popupShoutDown(){
@@ -227,4 +229,61 @@ public class HomeActivity extends AppCompatActivity {
         AlertDialog dialog = builder.create();
         dialog.show();
     }
+
+    private void initGoogleAPIClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(HomeActivity.this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult( requestCode, resultCode, data );
+        switch (requestCode) {
+            case REQUEST_CHECK_SETTINGS:
+                switch (resultCode) {
+                    case RESULT_OK:
+                        checkAppProviders();
+                        break;
+                    case RESULT_CANCELED:
+                        endRunningServiceCall();
+                        Util.warningDialog( getResources().getString( R.string.warning_gps ), HomeActivity.this );
+                        break;
+                }
+                break;
+        }
+    }
+
+    private void checkGpsSettings() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(Util.getUserLocation());
+
+        Task<LocationSettingsResponse> task = LocationServices.getSettingsClient(this).checkLocationSettings(builder.build());
+
+        task.addOnCompleteListener( new OnCompleteListener<LocationSettingsResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+                try {
+                    LocationSettingsResponse response = task.getResult(ApiException.class);
+                    checkAppProviders();
+
+                }catch (ApiException e){
+                    switch (e.getStatusCode()) {
+                        case LocationSettingsStatusCodes.SUCCESS:
+                            checkAppProviders();
+                            break;
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                            Util.showGpsDialog(e,HomeActivity.this);
+                            break;
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            // Location settings are not satisfied. However, we have no way to fix the
+                            // settings so we won't show the dialog.
+                            break;
+                    }
+                }
+            }
+        } );
+    }
+
 }
