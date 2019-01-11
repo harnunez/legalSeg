719774c216df034ddcb7f2a64e288138da4c9e54
jlopez <jonas_lopez@intranet.itresources>
2019-01-11 10:07:15 -0300

    Add userLocation & gpsDialog methods to Utils

--- app/src/main/java/com/example/gabriela/legalsecurityandroid/Utils/Util.java
+++ app/src/main/java/com/example/gabriela/legalsecurityandroid/Utils/Util.java
@@ -3,6 +3,7 @@ package com.example.gabriela.legalsecurityandroid.Utils;
 import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.IntentSender;
 import android.location.LocationManager;
 import android.media.MediaPlayer;
 import android.os.Build;
@@ -12,11 +13,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 
 import com.example.gabriela.legalsecurityandroid.R;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationRequest;
 
 public class Util extends AppCompatActivity {
     public static MediaPlayer alarm;
     private static AlertDialog.Builder dlgAlert;
     private static AlertDialog.Builder dlgWarning;
+    private static final int REQUEST_CHECK_SETTINGS = 0x1;
 
     public static void  alertError(String message, final Context context) {
         dlgAlert  = new AlertDialog.Builder(context);
@@ -57,13 +62,26 @@ public class Util extends AppCompatActivity {
         return locationManager.isProviderEnabled( LocationManager.GPS_PROVIDER );
     }
 
-    public static boolean isPasiveProviderEnable(Context context){
-        Activity activity = (Activity) context;
-        LocationManager locationManager = (LocationManager) activity.getSystemService( Context.LOCATION_SERVICE );
-        return locationManager.isProviderEnabled( LocationManager.PASSIVE_PROVIDER );
-    }
-
     public static boolean fieldIsEmpty(EditText text) {
         return text.getText().toString().isEmpty();
     }
+
+    public static LocationRequest getUserLocation(){
+        LocationRequest locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(30 * 1000)
+                .setFastestInterval(5 * 1000);
+        return locationRequest;
+    }
+
+    public static void showGpsDialog(ApiException e, Context context){
+        try {
+            Activity activity = (Activity) context;
+            ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+            resolvableApiException.startResolutionForResult(activity, REQUEST_CHECK_SETTINGS);
+        } catch (IntentSender.SendIntentException exception) {
+            alertError("No se pudo obtener obtener los permisos de Google" , context);
+        }
+    }
+
 }
