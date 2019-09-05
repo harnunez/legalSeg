5a3f58f77dd1a612a03460f8fb850d4d9ef68f06
Ninibeth_Parra <ninibeth_parra@itrsa.com.ar>
2019-09-05 16:08:23 -0300

    fixes

--- app/src/main/java/com/example/gabriela/legalsecurityandroid/activities/LoginActivity.java
+++ app/src/main/java/com/example/gabriela/legalsecurityandroid/activities/LoginActivity.java
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatEditText;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -29,8 +30,8 @@ import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
     private Button login;
-    private EditText user;
-    private EditText password;
+    private AppCompatEditText user;
+    private AppCompatEditText password;
     private ProgressBar loading;
     private ProgressIndicator progressIndicator;
     private boolean serviceInstanceBeenCalled = false;
